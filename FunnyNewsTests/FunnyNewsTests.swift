7bb7ca5be29669793a0e02b801969ae55fdb262e
Jovins <jovinscoder@gmail.com>
2025-05-08 11:44:50 +0800

    update code

---
+++ FunnyNewsTests/FunnyNewsTests.swift
@@ -0,0 +1,17 @@
+//
+//  FunnyNewsTests.swift
+//  FunnyNewsTests
+//
+//  Created by Jovins on 2025/4/18.
+//
+
+import Testing
+@testable import FunnyNews
+
+struct FunnyNewsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
